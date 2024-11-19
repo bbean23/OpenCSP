f83ff2cf3a0b7154a42db02ade1238b42f3e952e
bbean <bbean@sandia.gov>
2024-11-19 15:24:45 -0700

    migrating image processors in the __init__.py scripts

---
+++ contrib/common/lib/cv/spot_analysis/image_processor/__init__.py
@@ -0,0 +1,22 @@
+from contrib.common.lib.cv.spot_analysis.image_processor.CustomSimpleImageProcessor import CustomSimpleImageProcessor
+from contrib.common.lib.cv.spot_analysis.image_processor.InpaintImageProcessor import InpaintImageProcessor
+from contrib.common.lib.cv.spot_analysis.image_processor.PowerpointImageProcessor import PowerpointImageProcessor
+from contrib.common.lib.cv.spot_analysis.image_processor.SaveToFileImageProcessor import SaveToFileImageProcessor
+from contrib.common.lib.cv.spot_analysis.image_processor.StabilizationImageProcessor import StabilizationImageProcessor
+from contrib.common.lib.cv.spot_analysis.image_processor.TargetBoardLocatorImageProcessor import (
+    TargetBoardLocatorImageProcessor,
+)
+from contrib.common.lib.cv.spot_analysis.image_processor.ViewAnnotationsImageProcessor import (
+    ViewAnnotationsImageProcessor,
+)
+
+# Make these classes available when importing cv.spot_analysis.image_processor.*
+__all__ = [
+    'CustomSimpleImageProcessor',
+    'InpaintImageProcessor',
+    'PowerpointImageProcessor',
+    'SaveToFileImageProcessor',
+    'StabilizationImageProcessor',
+    'TargetBoardLocatorImageProcessor',
+    'ViewAnnotationsImageProcessor',
+]
--- opencsp/common/lib/cv/spot_analysis/image_processor/__init__.py
+++ opencsp/common/lib/cv/spot_analysis/image_processor/__init__.py
@@ -11,17 +11,14 @@ from opencsp.common.lib.cv.spot_analysis.image_processor.AverageByGroupImageProc
     AverageByGroupImageProcessor,
 )
 from opencsp.common.lib.cv.spot_analysis.image_processor.BcsLocatorImageProcessor import BcsLocatorImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.ColorspaceImageProcessor import ColorspaceImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.ConvolutionImageProcessor import ConvolutionImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.CroppingImageProcessor import CroppingImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.CustomSimpleImageProcessor import CustomSimpleImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.EchoImageProcessor import EchoImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.ExposureDetectionImageProcessor import (
     ExposureDetectionImageProcessor,
 )
 from opencsp.common.lib.cv.spot_analysis.image_processor.FalseColorImageProcessor import FalseColorImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.HotspotImageProcessor import HotspotImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.InpaintImageProcessor import InpaintImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.LogScaleImageProcessor import LogScaleImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.NullImageSubtractionImageProcessor import (
     NullImageSubtractionImageProcessor,
@@ -29,20 +26,10 @@ from opencsp.common.lib.cv.spot_analysis.image_processor.NullImageSubtractionIma
 from opencsp.common.lib.cv.spot_analysis.image_processor.PopulationStatisticsImageProcessor import (
     PopulationStatisticsImageProcessor,
 )
-from opencsp.common.lib.cv.spot_analysis.image_processor.PowerpointImageProcessor import PowerpointImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.RewindImageProcessor import RewindImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.SaveToFileImageProcessor import SaveToFileImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.StabilizationImageProcessor import StabilizationImageProcessor
 from opencsp.common.lib.cv.spot_analysis.image_processor.SupportingImagesCollectorImageProcessor import (
     SupportingImagesCollectorImageProcessor,
 )
-from opencsp.common.lib.cv.spot_analysis.image_processor.TargetBoardLocatorImageProcessor import (
-    TargetBoardLocatorImageProcessor,
-)
 from opencsp.common.lib.cv.spot_analysis.image_processor.View3dImageProcessor import View3dImageProcessor
-from opencsp.common.lib.cv.spot_analysis.image_processor.ViewAnnotationsImageProcessor import (
-    ViewAnnotationsImageProcessor,
-)
 from opencsp.common.lib.cv.spot_analysis.image_processor.ViewCrossSectionImageProcessor import (
     ViewCrossSectionImageProcessor,
 )
@@ -54,25 +41,16 @@ __all__ = [
     'AbstractVisualizationImageProcessor',
     'AverageByGroupImageProcessor',
     'BcsLocatorImageProcessor',
-    'ColorspaceImageProcessor',
     'ConvolutionImageProcessor',
     'CroppingImageProcessor',
-    'CustomSimpleImageProcessor',
     'EchoImageProcessor',
     'ExposureDetectionImageProcessor',
     'FalseColorImageProcessor',
     'HotspotImageProcessor',
-    'InpaintImageProcessor',
     'LogScaleImageProcessor',
     'NullImageSubtractionImageProcessor',
     'PopulationStatisticsImageProcessor',
-    'PowerpointImageProcessor',
-    'RewindImageProcessor',
-    'SaveToFileImageProcessor',
-    'StabilizationImageProcessor',
     'SupportingImagesCollectorImageProcessor',
-    'TargetBoardLocatorImageProcessor',
     'View3dImageProcessor',
-    'ViewAnnotationsImageProcessor',
     'ViewCrossSectionImageProcessor',
 ]
