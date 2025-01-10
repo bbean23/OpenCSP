bf3581181815b46a97d5e74bd7e22b768821e0f8
bbean <bbean@sandia.gov>
2025-01-09 20:54:30 -0700

    fixed Pxy.angle_from() and Pxy.distance()

--- opencsp/common/lib/geometry/Pxy.py
+++ opencsp/common/lib/geometry/Pxy.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.typing as npt
 
 from opencsp.common.lib.geometry.Vxy import Vxy
+import opencsp.common.lib.tool.log_tools as lt
 
 
 class Pxy(Vxy):
@@ -15,6 +16,18 @@ class Pxy(Vxy):
     def distance(self, data_in: "Pxy") -> npt.NDArray[np.float_]:
         """Calculates the euclidian distance between this point and the data_in point."""
         self._check_is_Vxy(data_in)
+
+        # broadcast input point to the same number of points as self
+        if len(data_in) != len(self):
+            if len(data_in) == 1:
+                data_in = Pxy([[data_in.x[0]] * len(self), [data_in.y[0]] * len(self)])
+            else:
+                lt.error_and_raise(
+                    ValueError,
+                    "Error in Pxy.angle_from(): "
+                    + f"'data_in' must be of length 1, or the same length as the destination instance ({len(self)=}).",
+                )
+
         return (self - data_in).magnitude()
 
     def angle_from(self, origin: "Pxy") -> npt.NDArray[np.float_]:
@@ -23,6 +36,18 @@ class Pxy(Vxy):
         given origin point.
         """
         self._check_is_Vxy(origin)
+
+        # broadcast input point to the same number of points as self
+        if len(origin) != len(self):
+            if len(origin) == 1:
+                origin = Pxy([[origin.x[0]] * len(self), [origin.y[0]] * len(self)])
+            else:
+                lt.error_and_raise(
+                    ValueError,
+                    "Error in Pxy.angle_from(): "
+                    + f"'origin' must be of length 1, or the same length as the destination instance ({len(self)=}).",
+                )
+
         return (self - origin).angle()
 
     def as_Vxy(self):
