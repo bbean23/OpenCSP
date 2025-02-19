5294274f26695421443d109166ab3b6b57627648
Braden <bsmith4@sandia.gov>
2025-02-19 15:05:02 -0700

    Instantiation of example

---
+++ contrib/app/sofast/sofast_temperature_analysis.py
@@ -0,0 +1,10 @@
+"""Python script that provides an example of how to process SOFAST data
+of the Sandia temperature chamber experiment. In this experiment, a CSP
+mirror was placed in a large temerature chamber. An installation of SOFAST
+was set up outside of the temperature chamber so that when the chamber
+doors were opened, SOFAST could immediately take a measurement. This was
+done for a range of temperatures from +5C to -50C. This example will walk
+a user through how to automatically process such data, how to automatically
+plot slope maps, and how to generate slope difference plots to show the
+change in slope from room temerature.
+"""
