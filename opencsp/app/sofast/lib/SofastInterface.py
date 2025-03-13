be5e95ad68e2c162eb3aaf31764a312b7deab664
Braden <bsmith4@sandia.gov>
2025-03-13 13:52:07 -0600

    MIS and MRS working.

--- opencsp/app/sofast/lib/SofastInterface.py
+++ opencsp/app/sofast/lib/SofastInterface.py
@@ -1,6 +1,10 @@
 import glob
 from os.path import join, dirname, abspath
 from dataclasses import dataclass
+import datetime as dt
+import time
+import threading
+
 import matplotlib.pyplot as plt
 
 from opencsp.app.sofast.lib.DefinitionFacet import DefinitionFacet
@@ -14,7 +18,7 @@ from opencsp.app.sofast.lib.SpatialOrientation import SpatialOrientation
 from opencsp.app.sofast.lib.SystemSofastFringe import SystemSofastFringe
 from opencsp.app.sofast.lib.SystemSofastFixed import SystemSofastFixed
 from opencsp.common.lib.camera.Camera import Camera
-from opencsp.common.lib.camera.ImageAcquisition_DCAM_mono import ImageAcquisition
+from opencsp.common.lib.camera.ImageAcquisitionAbstract import ImageAcquisitionAbstract
 from opencsp.common.lib.camera.image_processing import highlight_saturation
 from opencsp.common.lib.camera.LiveView import LiveView
 from opencsp.common.lib.deflectometry.ImageProjection import ImageProjection
@@ -28,139 +32,103 @@ import opencsp.common.lib.tool.log_tools as lt
 from opencsp.common.lib.tool.time_date_tools import current_date_time_string_forfile as timestamp
 
 
+@dataclass
+class SofastProcessingData:
+    # Common
+    image_acquisition: ImageAcquisitionAbstract = None
+    image_projection: ImageProjection = None
+    camera: Camera = None
+    facet_definition: DefinitionFacet = None
+    spatial_orientation: SpatialOrientation = None
+    # Sofast Fringe
+    display_shape: DisplayShape = None
+    surface_fringe: Surface2DParabolic = None
+    # Sofast Fixed
+    fixed_pattern_dot_locs: DotLocationsFixedPattern = None
+    surface_fixed: Surface2DParabolic = None
+    process_sofast_fixed: ProcessSofastFixed = None
+
+
+@dataclass
+class SofastFixedRunData:
+    origin: Vxy = None
+    pattern_width = 3
+    """Fixed pattern dot width, pixels"""
+    pattern_spacing = 6
+    """Fixed pattern dot spacing, pixels"""
+
+    def is_ready(self) -> bool:
+        if (self.origin is None) or (self.pattern_width is None) or (self.pattern_spacing is None):
+            return False
+        return True
+
+
+@dataclass
+class SofastCommonRunData:
+    measure_point_optic: Vxyz = None
+    dist_optic_screen: float = None
+    name_optic: str = None
+
+    def is_ready(self) -> bool:
+        if (self.measure_point_optic is None) or (self.dist_optic_screen is None) or (self.name_optic is None):
+            return False
+        return True
+
+
+@dataclass
+class Paths:
+    dir_save_fringe: str = ""
+    """Location to save Sofast Fringe measurement data"""
+    dir_save_fixed: str = ""
+    """Location to save Sofast Fixed measurement data"""
+    dir_save_fringe_calibration: str = ""
+    """Location to save Sofast Fringe calibration data"""
+
+
 class SofastInterface:
-    def __init__(self) -> "SofastInterface":
-        # Common sofast parameters
-        self.image_acquisition: ImageAcquisition = None
-        self.camera: Camera = None
-        self.facet_definition: DefinitionFacet = None
-        self.spatial_orientation: SpatialOrientation = None
-        self.measure_point_optic: Vxyz = None
-        self.dist_optic_screen: float = None
-        self.name_optic: str = None
-
+    def __init__(self, image_acquisition: ImageAcquisitionAbstract) -> "SofastInterface":
+        self.image_acquisition = image_acquisition
         self.image_projection = ImageProjection.instance()
-        self.res_plot = 0.002  # meters
-        """Resolution of slope map image (meters)"""
-        self.colorbar_limit = 7  # mrad
-        """Colorbar limits in slope magnitude plot (mrad)"""
 
-        # Sofast fringe specific
-        self.system_fringe: SystemSofastFringe = None
-        self.display_shape: DisplayShape = None
-        self.surface_fringe: Surface2DParabolic = None
-        self.timestamp_fringe_measurement = None
+        self.data_sofast_processing = SofastProcessingData()
+        self.data_sofast_common_run = SofastCommonRunData()
+        self.data_sofast_fixed_run = SofastFixedRunData()
+        self.paths = Paths
+        self.timestamp: dt.datetime = None
 
-        # Sofast fixed specific
         self.system_fixed: SystemSofastFixed = None
-        self.process_sofast_fixed: ProcessSofastFixed = None
-        self.fixed_pattern_dot_locs: DotLocationsFixedPattern = None
-        self.surface_fixed: Surface2DParabolic = None
-        self.origin: Vxy = None
-        self.timestamp_fixed_measurement = None
-        self.pattern_width = 3
-        """Fixed pattern dot width, pixels"""
-        self.pattern_spacing = 6
-        """Fixed pattern dot spacing, pixels"""
+        self.system_fringe: SystemSofastFringe = None
 
-        # Save directories
-        self.dir_save_fringe: str = ""
-        """Location to save Sofast Fringe measurement data"""
-        self.dir_save_fixed: str = ""
-        """Location to save Sofast Fixed measurement data"""
-        self.dir_save_fringe_calibration: str = ""
-        """Location to save Sofast Fringe calibration data"""
-
-    def run(self) -> None:
+    def run_cli(self) -> None:
         """Runs command line Sofast"""
-        self.func_user_input()
+        self._func_user_input()
 
-    def set_common_data(
-        self,
-        image_acquisition: ImageAcquisition,
-        image_projection: ImageProjection,
-        camera: Camera,
-        facet_definition: DefinitionFacet,
-        spatial_orientation: SpatialOrientation,
-        measure_point_optic: Vxyz,
-        dist_optic_screen: float,
-        name_optic: str,
-    ) -> None:
+    def set_sofast_processing_data(self, data: SofastProcessingData) -> None:
         """Sets common parametes for Sofast Fringe and Fixed
 
         Parameters
         ----------
-        image_acquisition : ImageAcquisition
-            ImageAcquisition object
-        image_projection : ImageProjection
-            Image projection object
-        camera : Camera
-            Camera calibration object
-        facet_definition : DefinitionFacet
-            Facet definition object
-        spatial_orientation : SpatialOrientation
-            Calibrated SpatialOrientation object
-        measure_point_optic : Vxyz
-            Measure point location on optic
-        dist_optic_screen : float
-            Distance from measure point to screen center
-        name_optic : str
-            Name of optic
+        data : SofastProcessingData
+            SofastProcessingData object
         """
-        self.image_acquisition = image_acquisition
-        self.image_projection = image_projection
-        self.camera = camera
-        self.facet_definition = facet_definition
-        self.spatial_orientation = spatial_orientation
-        self.measure_point_optic = measure_point_optic
-        self.dist_optic_screen = dist_optic_screen
-        self.name_optic = name_optic
+        self.data_sofast_processing = data
 
-    def set_sofast_fringe_data(
-        self, display_shape: DisplayShape, fringes: Fringes, surface_fringe: Surface2DParabolic
-    ) -> None:
-        """Loads Sofast Fringe specific objects
-
-        Parameters
-        ----------
-        display_shape : DisplayShape
-            Calibrated DisplayShape object
-        fringes : Fringes
-            Fringe objects to display
-        surface_fringe : Surface2DParabolic
-            Surface to use when processing Sofast Fringe data
-        """
+    def initialize_sofast_fringe(self, fringes: Fringes) -> None:
+        """Initializes sofast fringe system"""
         self.system_fringe = SystemSofastFringe(self.image_acquisition)
         self.system_fringe.set_fringes(fringes)
-        self.display_shape = display_shape
-        self.surface_fringe = surface_fringe
 
-    def set_sofast_fixed_data(
-        self, fixed_pattern_dot_locs: DotLocationsFixedPattern, origin: Vxy, surface_fixed: Surface2DParabolic
-    ) -> None:
-        """Loads Sofast Fixed specific objects
-
-        Parameters
-        ----------
-        fixed_pattern_dot_locs : DotLocationsFixedPattern
-            Calibrated dot locations object
-        origin : Vxy
-            Origin dot location in image, pixels
-        surface_fixed : Surface2DParabolic
-            Surface to use when processing Sofast Fringe data
-        """
-        self.system_fixed = SystemSofastFixed(self.image_acquisition)
-        self.system_fixed.set_pattern_parameters(self.pattern_width, self.pattern_spacing)
-        self.fixed_pattern_dot_locs = fixed_pattern_dot_locs
-        self.origin = origin
-        self.surface_fixed = surface_fixed
-
-        self.process_sofast_fixed = ProcessSofastFixed(self.spatial_orientation, self.camera, fixed_pattern_dot_locs)
+    def initialize_sofast_fixed(self) -> None:
+        """Initializes sofast fixed system"""
+        if self._check_fixed_run_data_loaded():
+            self.system_fixed = SystemSofastFixed(self.image_acquisition)
+            self.system_fixed.set_pattern_parameters(
+                self.data_sofast_fixed_run.pattern_width, self.data_sofast_fixed_run.pattern_spacing
+            )
 
     def func_run_fringe_measurement(self) -> None:
         """Runs sofast fringe measurement"""
         lt.info(f"{timestamp():s} Starting Sofast Fringe measurement")
-        self.timestamp_fringe_measurement = timestamp()
 
         def _on_done():
             lt.info(f"{timestamp():s} Completed Sofast Fringe measurement")
@@ -171,7 +139,6 @@ class SofastInterface:
     def func_run_fixed_measurement(self) -> None:
         """Runs Sofast Fixed measurement"""
         lt.info(f"{timestamp():s} Starting Sofast Fixed measurement")
-        self.timestamp_fixed_measurement = timestamp()
 
         def _f1():
             lt.info(f"{timestamp():s} Completed Sofast Fixed measurement")
@@ -262,9 +229,12 @@ class SofastInterface:
     def func_save_measurement_fringe(self):
         """Saves measurement to HDF file"""
         measurement = self.system_fringe.get_measurements(
-            self.measure_point_optic, self.dist_optic_screen, self.name_optic
+            self.data_sofast_common_run.measure_point_optic,
+            self.data_sofast_common_run.dist_optic_screen,
+            self.data_sofast_common_run.name_optic,
         )[0]
-        file = f"{self.dir_save_fringe:s}/{self.timestamp_fringe_measurement:s}_measurement_fringe.h5"
+        file_timestamp = self.timestamp.strftime(r"%Y-%m-%d_%H_%M_%S.%f")
+        file = f"{self.paths.dir_save_fringe:s}/{file_timestamp:s}_measurement_fringe.h5"
         measurement.save_to_hdf(file)
         self.system_fringe.calibration.save_to_hdf(file)
         self.system_fringe.run_next_in_queue()
@@ -272,19 +242,23 @@ class SofastInterface:
     def func_save_measurement_fixed(self):
         """Save fixed measurement files"""
         measurement = self.system_fixed.get_measurement(
-            self.measure_point_optic, self.dist_optic_screen, self.origin, name=self.name_optic
+            self.data_sofast_common_run.measure_point_optic,
+            self.data_sofast_common_run.dist_optic_screen,
+            self.data_sofast_fixed_run.origin,
+            name=self.data_sofast_common_run.name_optic,
         )
-        measurement.save_to_hdf(f"{self.dir_save_fixed:s}/{self.timestamp_fixed_measurement:s}_measurement_fixed.h5")
+        file_timestamp = self.timestamp.strftime(r"%Y-%m-%d_%H_%M_%S.%f")
+        measurement.save_to_hdf(f"{self.paths.dir_save_fixed:s}/{file_timestamp:s}_measurement_fixed.h5")
         self.system_fixed.run_next_in_queue()
 
     def func_load_last_sofast_fringe_image_cal(self):
         """Loads last ImageCalibration object"""
         # Find file
-        files = glob.glob(join(self.dir_save_fringe_calibration, "image_calibration_scaling*.h5"))
+        files = glob.glob(join(self.paths.dir_save_fringe_calibration, "image_calibration_scaling*.h5"))
         files.sort()
 
         if len(files) == 0:
-            lt.error(f"No previous calibration files found in {self.dir_save_fringe_calibration}")
+            lt.error(f"No previous calibration files found in {self.paths.dir_save_fringe_calibration}")
             return
 
         # Get latest file and set
@@ -295,11 +269,11 @@ class SofastInterface:
 
     def func_gray_levels_cal(self):
         """Runs gray level calibration sequence"""
-        file = join(self.dir_save_fringe_calibration, f"image_calibration_scaling_{timestamp():s}.h5")
+        file = join(self.paths.dir_save_fringe_calibration, f"image_calibration_scaling_{timestamp():s}.h5")
         self.system_fringe.run_gray_levels_cal(
             ImageCalibrationScaling,
             file,
-            on_processed=self.func_user_input,
+            on_processed=self._func_user_input,
             on_processing=self.func_show_crosshairs_fringe,
         )
 
@@ -311,32 +285,50 @@ class SofastInterface:
         plt.show()
 
     def show_live_view(self):
-        """Shows live vieew window"""
+        """Shows live view window"""
         LiveView(self.image_acquisition)
 
-    def func_user_input(self):
-        """Waits for user input"""
-        retval = input("Input: ")
+    def _check_fixed_run_data_loaded(self) -> bool:
+        if not self.data_sofast_fixed_run.is_ready():
+            lt.error(f"{timestamp()} Sofast Fixed processing data not fully loaded")
+            return False
+        return True
 
-        lt.debug(f"{timestamp():s} user input: {retval:s}")
-
-        try:
-            self._run_given_input(retval)
-        except Exception as error:
-            lt.error(repr(error))
-            self.func_user_input()
+    def _check_common_run_data_loaded(self) -> bool:
+        if not self.data_sofast_common_run.is_ready():
+            lt.error(f"{timestamp()} Sofast Fringe processing data not fully loaded")
+            return False
+        return True
 
     def _check_fringe_system_loaded(self) -> bool:
         if self.system_fringe is None:
-            lt.error(f"{timestamp()} No Sofast Fringe system loaded")
+            lt.error(f"{timestamp()} Sofast Fringe system not initizlized")
+            return False
+        if not self._check_common_run_data_loaded():
             return False
         return True
 
     def _check_fixed_system_loaded(self) -> bool:
         if self.system_fixed is None:
-            lt.error(f"{timestamp()} No Sofast Fixed system loaded")
+            lt.error(f"{timestamp()} Sofast Fixed system not initialized")
+            return False
+        if not self._check_common_run_data_loaded():
+            return False
+        if not self._check_fixed_run_data_loaded():
             return False
         return True
+
+    def _func_user_input(self):
+        """Function that requests and processes user input"""
+        # Get user input
+        retval = input("> ")
+        self.timestamp = dt.datetime.now()
+        lt.debug(f"{self.timestamp:s} user input: {retval:s}")
+
+        try:
+            self._run_given_input(retval)
+        except Exception as error:
+            lt.error(repr(error))
 
     def _run_given_input(self, retval: str) -> None:
         """Runs the given command"""
@@ -356,7 +348,7 @@ class SofastInterface:
             print("im         show image from camera.")
             print("lv         shows camera live view")
             print("cross      show crosshairs")
-            self.func_user_input()
+            self._func_user_input()
         elif retval == "mrp":
             lt.info(f"{timestamp()} Running Sofast Fringe measurement and processing/saving data")
             if self._check_fringe_system_loaded():
@@ -365,12 +357,12 @@ class SofastInterface:
                     self.func_show_crosshairs_fringe,
                     self.func_process_sofast_fringe_data,
                     self.func_save_measurement_fringe,
-                    self.func_user_input,
+                    self._func_user_input,
                 ]
                 self.system_fringe.set_queue(funcs)
                 self.system_fringe.run()
             else:
-                self.func_user_input()
+                self._func_user_input()
         # Run fringe measurement and save
         elif retval == "mrs":
             lt.info(f"{timestamp()} Running Sofast Fringe measurement and saving data")
@@ -379,12 +371,12 @@ class SofastInterface:
                     self.func_run_fringe_measurement,
                     self.func_show_crosshairs_fringe,
                     self.func_save_measurement_fringe,
-                    self.func_user_input,
+                    self._func_user_input,
                 ]
                 self.system_fringe.set_queue(funcs)
                 self.system_fringe.run()
             else:
-                self.func_user_input()
+                self._func_user_input()
         # Run fixed measurement and process/save
         elif retval == "mip":
             lt.info(f"{timestamp()} Running Sofast Fixed measurement and processing/saving data")
@@ -393,26 +385,26 @@ class SofastInterface:
                     self.func_run_fixed_measurement,
                     self.func_process_sofast_fixed_data,
                     self.func_save_measurement_fixed,
-                    self.func_user_input,
+                    self._func_user_input,
                 ]
                 self.system_fixed.set_queue(funcs)
                 self.system_fixed.run()
             else:
-                self.func_user_input()
+                self._func_user_input()
         # Run fixed measurement and save
         elif retval == "mis":
             lt.info(f"{timestamp()} Running Sofast Fixed measurement and saving data")
             if self._check_fixed_system_loaded():
-                funcs = [self.func_run_fixed_measurement, self.func_save_measurement_fixed, self.func_user_input]
+                funcs = [self.func_run_fixed_measurement, self.func_save_measurement_fixed, self._func_user_input]
                 self.system_fixed.set_queue(funcs)
                 self.system_fixed.run()
             else:
-                self.func_user_input()
+                self._func_user_input()
         # Calibrate exposure time
         elif retval == "ce":
             lt.info(f"{timestamp()} Calibrating camera exposure")
             self.image_acquisition.calibrate_exposure()
-            self.func_user_input()
+            self._func_user_input()
         # Calibrate response
         elif retval == "cr":
             lt.info(f"{timestamp()} Calibrating camera-projector response")
@@ -421,13 +413,13 @@ class SofastInterface:
                 self.system_fringe.set_queue(funcs)
                 self.system_fringe.run()
             else:
-                self.func_user_input()
+                self._func_user_input()
         # Load last fringe calibration file
         elif retval == "lr":
             lt.info(f"{timestamp()} Loading response calibration")
             if self._check_fringe_system_loaded():
                 self.func_load_last_sofast_fringe_image_cal()
-            self.func_user_input()
+            self._func_user_input()
         # Quit
         elif retval == "q":
             lt.info(f"{timestamp():s} quitting")
@@ -439,15 +431,15 @@ class SofastInterface:
         # Show single camera image
         elif retval == "im":
             self.show_cam_image()
-            self.func_user_input()
+            self._func_user_input()
         # Show camera live view
         elif retval == "lv":
             self.show_live_view()
-            self.func_user_input()
+            self._func_user_input()
         # Project crosshairs
         elif retval == "cross":
             self.image_projection.show_crosshairs()
-            self.func_user_input()
+            self._func_user_input()
         else:
             lt.error(f"{timestamp()} Command, {retval}, not recognized")
-            self.func_user_input()
+            self._func_user_input()
